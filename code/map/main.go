fded8470622fdaf2e178c1b797c9059f27a8a744
Pritam Sukumar <pritamps@gmail.com>
2018-05-29 21:16:42 +0530

    Finished Maps section

--- code/map/main.go
+++ code/map/main.go
@@ -5,11 +5,27 @@ import (
 )
 
 func main() {
-	// Declare a map
+
+	// Declare a map - Method 1
 	colors := map[string]string{
 		"green": "00FF00",
 		"red":   "FF0000",
 		"blue":  "0000FF",
 	}
-	fmt.Println(colors["green"])
+
+	fmt.Println(colors)
+
+	// Declare a map - Method 2
+	moreColors := make(map[string]string)
+	moreColors["violet"] = "aa00bb"
+
+	printMap(colors)
+
+}
+
+func printMap(c map[string]string) {
+
+	for k, v := range c {
+		fmt.Println(k, v)
+	}
 }
