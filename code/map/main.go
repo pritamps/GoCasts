72389e1f3b973abe8138c5adf6e125c9f8b9037b
Pritam Sukumar <pritamps@gmail.com>
2018-05-29 14:51:09 +0530

    ADded map

--- code/map/main.go
+++ code/map/main.go
@@ -1,22 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	// Declare a map
 	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-		"white": "#ffffff",
+		"green": "00FF00",
+		"red":   "FF0000",
+		"blue":  "0000FF",
 	}
-
-	colors["yellow"] = "laksjdf"
-	delete(colors, "yellow")
-
-	printMap(colors)
-}
-
-func printMap(c map[string]string) {
-	for color, hex := range c {
-		fmt.Println("Hex code for", color, "is", hex)
-	}
+	fmt.Println(colors["green"])
 }
